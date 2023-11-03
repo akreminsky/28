aa2f2ed5f8525d56200cead7679c5b00149b49a7
akreminsky <akreminsky2023@yandex.ru>
2023-11-03 19:12:35 +0300

    2 without tests

--- 4/main.py
+++ 4/main.py
@@ -1,13 +1,16 @@
-def start_impulse(arr): arr.sort()
+def sort_and_max_center(arr, N): Создаем
 
 
-start = [arr[0]]
-left = arr[1:len(arr) // 2 + 1]
-right = arr[len(arr) // 2 + 1:][::-1]
-start.extend(left)
-start.extend(right)
-return start
-
+случайный
+массив
+чисел
+random_arr = generate_random_array(N)
+sorted_arr = sorted(random_arr)
+arr.append(sorted_arr[0])
+for i in reversed(range(len(sorted_arr))): arr.insert(0, sorted_arr[i])
+max_index = len(arr) // 2
+result = arr[:max_index + 1] + arr[max_index:]
+return result
 arr = [1, 2, 3, 4, 5, 6, 7]
 start = start_impulse(arr)
 print(start)
