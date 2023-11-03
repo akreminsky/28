61f441fa2e136fdefbf90b5104173d46095060e9
akreminsky <akreminsky2023@yandex.ru>
2023-11-03 18:41:46 +0300

    2 without tests

--- 3/main.py
+++ 3/main.py
@@ -5,6 +5,7 @@ def ConquestCampaign(N: int, M: int, L: int, battalion: List[int]):
     in_val = battalion.copy()
 
     while True:
+        counter += 1
         list_batallions = set()
         for i in range(len(in_val)):
             if i % 2 != 0:
@@ -16,13 +17,13 @@ def ConquestCampaign(N: int, M: int, L: int, battalion: List[int]):
         for i in range(len(list_batallions)):
             if list_b[i][0] - 1 != 0 and list_b[i][0] - 1 <= M and list_b[i][1] != 0 and list_b[i][1] <= N:
                 list_batallions_copy.add(tuple([list_b[i][0] - 1, list_b[i][1]]))
-            if list_b[i][0] - 1 != 0 and list_b[i][0] - 1 <= M and list_b[i][1] != 0 and list_b[i][1] <= N:
+            if list_b[i][0] - 1 != 0 and list_b[i][0] - 1 <= N and list_b[i][1] != 0 and list_b[i][1] <= M:
                 list_batallions_copy.add(tuple([list_b[i][0] - 1, list_b[i][1]]))
-            if list_b[i][0] + 1 != 0 and list_b[i][0] + 1 <= M and list_b[i][1] != 0 and list_b[i][1] <= N:
+            if list_b[i][0] + 1 != 0 and list_b[i][0] + 1 <= N and list_b[i][1] != 0 and list_b[i][1] <= M:
                 list_batallions_copy.add(tuple([list_b[i][0] + 1, list_b[i][1]]))
-            if list_b[i][0] != 0 and list_b[i][0] <= M and list_b[i][1] - 1 != 0 and list_b[i][1] - 1 <= N:
+            if list_b[i][0] != 0 and list_b[i][0] - 1 <= N and list_b[i][1] - 1 != 0 and list_b[i][1] - 1 <= M:
                 list_batallions_copy.add(tuple([list_b[i][0], list_b[i][1] - 1]))
-            if list_b[i][0] != 0 and list_b[i][0] <= M and list_b[i][1] + 1 != 0 and list_b[i][1] + 1 <= N:
+            if list_b[i][0] != 0 and list_b[i][0] - 1 <= N and list_b[i][1] + 1 != 0 and list_b[i][1] + 1 <= M:
                 list_batallions_copy.add(tuple([list_b[i][0], list_b[i][1] + 1]))
 
         list_a = list(list_batallions_copy)
@@ -30,10 +31,7 @@ def ConquestCampaign(N: int, M: int, L: int, battalion: List[int]):
         for i in range(len(list_a)):
             result.append(list_a[i][0])
             result.append(list_a[i][1])
-        if result == in_val:
+        if sorted(result) == sorted(in_val):
             return counter
-        counter += 1
+
         in_val = result.copy()
-
-
-print(ConquestCampaign(3, 4, 0, [2, 2, 3, 4]))
--- 3/test_main.py
+++ 3/test_main.py
@@ -1,6 +1,14 @@
 import unittest
 
-from main import squirrel
+from main import ConquestCampaign
 
-class FactTests(unittest.TestCase):
-    def test_fact_first_num(self):
+
+class ConquestTests(unittest.TestCase):
+    def test_conq_small(self):
+        self.assertEqual(ConquestCampaign(3, 2, 2, [2, 2]), 3)
+        self.assertEqual(ConquestCampaign(2, 2, 2, [2, 2, 1, 1]), 2)
+        self.assertEqual(ConquestCampaign(1, 1, 2, [1, 1]), 1)
+
+    def test_conq_large_batallions(self):
+        self.assertEqual(ConquestCampaign(2, 2, 2, [1, 1, 1, 2, 2, 1, 2, 2]), 1)
+        self.assertEqual(ConquestCampaign(2, 2, 2, [1, 2, 2, 1, 2, 2]), 2)
